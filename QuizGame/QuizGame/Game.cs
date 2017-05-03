6e3e88eb7a38613b3d66e2ce444a31a8e8e73b74
silentgolem <liamlogan16@yahoo.co.uk>
2017-05-03 15:57:06 +0100

    Data layer

--- QuizGame/QuizGame/Game.cs
+++ QuizGame/QuizGame/Game.cs
@@ -24,17 +24,7 @@ namespace QuizGame
 
         private List<Question> GetQuestions()
         {
-            List<Question> Output = new List<Question>();
-            string filePath = "questions.txt";
-            string[] details = new string[File.ReadAllLines(filePath).Count()];
-
-            details = File.ReadAllLines(filePath);
-            foreach (string line in details)
-            {
-                string[] questionDetails = line.Split(',');
-                Question newQ = new Question(questionDetails[0], questionDetails[1], questionDetails[2], questionDetails[3], questionDetails[4], questionDetails[5]);
-                Output.Add(newQ);
-            }
+            List<Question> Output = RetrieveFromFile.GetQuestions();
             return Output;
         }
 
---
+++ QuizGame/QuizGame/RetrieveFromFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    class RetrieveFromFile
+    {
+        public static List<Question> GetQuestions()
+        {
+            List<Question> Output = new List<Question>();
+            string filePath = "questions.txt";
+            string[] details = new string[File.ReadAllLines(filePath).Count()];
+
+            details = File.ReadAllLines(filePath);
+            foreach (string line in details)
+            {
+                string[] questionDetails = line.Split(',');
+                Question newQ = new Question(questionDetails[0], questionDetails[1], questionDetails[2], questionDetails[3], questionDetails[4], questionDetails[5]);
+                Output.Add(newQ);
+            }
+            return Output;
+        }
+    }
+}
